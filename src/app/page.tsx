cb4396f1182edc7ba8484189e2cb0dbccde4c9aa
Dinesh Umasankar <dqu5021@psu.edu>
2023-09-03 18:55:57 -0400

    Initialize ShadCN

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
-    <p>This is a sample change to see if they are propogated through CloudFlare dns and Vercel</p>
+    <>
+      <p>This is a sample change to see if they are propogated through CloudFlare dns and Vercel</p>
+      <Button>ShadCN Test</Button>
+    </>
   )
 }
