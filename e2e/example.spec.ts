55c888b60baecfb21fc3a850a0300285855bfc9b
Dinesh Umasankar <dinesh71uma@gmail.com>
2023-09-21 23:22:00 -0400

    e2e testing on push/pull playwright
    - Testing via containers to run Playwright Tests

--- e2e/example.spec.ts
+++ e2e/example.spec.ts
@@ -1,9 +1,9 @@
 import { test, expect } from '@playwright/test'
 
-test('should navigate to home page',async ({ page }) => {
+test('should navigate to home page', async ({ page }) => {
     // Start from the index page (the baseURL is set via playwright.config.ts)
     await page.goto('http://localhost:3000')
-    
+
     // Find an element with the text 'Shad CN Test' and click on it
     await page.click('text=ShadCN Test')
 
