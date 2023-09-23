68f28c0f5c48b22f372509db827846eb2062849c
Dinesh Umasankar <dinesh71uma@gmail.com>
2023-09-22 22:43:15 -0400

    fix: Deployment URL is not being hit when Playwright E2E Tests are run
    - added Deployment URL via env variable in URL attribute of playwright config

--- playwright.config.ts
+++ playwright.config.ts
@@ -4,42 +4,42 @@ export default defineConfig({
     // Runs your local dev server before starting the tests
     webServer: {
         command: 'pnpm run dev',
-        url: 'http://127.0.0.1:3000',
+        url: process.env.PLAYWRIGHT_TEST_BASE_URL || 'http://127.0.0.1:3000',
         reuseExistingServer: !process.env.CI,
         stdout: 'ignore',
         stderr: 'pipe',
-        timeout: 120 * 1000,
+        timeout: 120 * 1000, // Waiting two minutes (maximum) until web server spins online
     },
     projects: [
         {
             name: 'chromium',
-            use: { ...devices['Desktop Chrome']}
+            use: { ...devices['Desktop Chrome'] }
         },
         {
             name: 'firefox',
-            use: { ...devices['Desktop Firefox']}
+            use: { ...devices['Desktop Firefox'] }
         },
         {
             name: 'webkit',
-            use: { ...devices['Desktop Safari']}
+            use: { ...devices['Desktop Safari'] }
         },
 
         /* Test against mobile view ports. */
         {
             name: 'Mobile Chrome',
-            use: { ...devices['Pixel 5']}
+            use: { ...devices['Pixel 5'] }
         },
         {
             name: 'Mobile Safari',
-            use: { ...devices['iPhone 12']}
+            use: { ...devices['iPhone 12'] }
         },
 
         /* Test against branded browsers. */
         {
             name: 'edge',
             use: {
-            ...devices['Desktop Edge'],
-            channel: 'msedge'
+                ...devices['Desktop Edge'],
+                channel: 'msedge'
             },
         },
     ]
