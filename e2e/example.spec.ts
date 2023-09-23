99fc0079bfae345cbf07b64906d87b54aa986c7c
Dinesh Umasankar <dinesh71uma@gmail.com>
2023-09-23 11:08:28 -0400

    fix: Update test to use baseURL

--- e2e/example.spec.ts
+++ e2e/example.spec.ts
@@ -2,7 +2,7 @@ import { test, expect } from '@playwright/test'
 
 test('should navigate to home page', async ({ page }) => {
     // Start from the index page (the baseURL is set via playwright.config.ts)
-    await page.goto('http://localhost:3000')
+    await page.goto('/')
 
     // Find an element with the text 'Shad CN Test' and click on it
     await page.click('text=ShadCN Test')
