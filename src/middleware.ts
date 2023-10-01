cbd028783193809c68086b70dd0fb78c4810a597
Dinesh Umasankar <dinesh71uma@gmail.com>
2023-09-30 23:52:46 -0400

    fix: sign-in routes being in wrong directory

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,8 +2,10 @@ import { authMiddleware } from "@clerk/nextjs";
 
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your middleware
-export default authMiddleware({});
+export default authMiddleware({
+  publicRoutes: ["/"]
+});
 
 export const config = {
-  matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/(api)(.*)'],
+  matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
 };
