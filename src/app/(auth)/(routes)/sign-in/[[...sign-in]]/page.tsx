edc4ad9d3aac2b0d40f4145d94facce80ecbc7c5
Dinesh Umasankar <dinesh71uma@gmail.com>
2023-10-02 10:36:40 -0400

    feat: Added SignIn/SignUp URLs on Clerk Pages

--- src/app/(auth)/(routes)/sign-in/[[...sign-in]]/page.tsx
+++ src/app/(auth)/(routes)/sign-in/[[...sign-in]]/page.tsx
@@ -1,5 +1,10 @@
 import { SignIn } from "@clerk/nextjs";
 
 export default function Page() {
-  return <SignIn afterSignInUrl={process.env.NEXT_PUBLIC_AFTER_SIGN_IN_URL} />
+  return (
+    <SignIn
+      afterSignInUrl={process.env.NEXT_PUBLIC_AFTER_SIGN_IN_URL}
+      signUpUrl={process.env.NEXT_PUBLIC_CLERK_SIGN_UP_URL}
+    />
+  )
 }
--- src/app/(auth)/(routes)/sign-up/[[...sign-up]]/page.tsx
+++ src/app/(auth)/(routes)/sign-up/[[...sign-up]]/page.tsx
@@ -1,5 +1,10 @@
 import { SignUp } from "@clerk/nextjs";
 
 export default function Page() {
-  return <SignUp afterSignUpUrl={process.env.NEXT_PUBLIC_AFTER_SIGN_UP_URL} />
+  return (
+    <SignUp
+      afterSignUpUrl={process.env.NEXT_PUBLIC_AFTER_SIGN_UP_URL}
+      signInUrl={process.env.NEXT_PUBLIC_CLERK_SIGN_IN_URL}
+    />
+  )
 }
