139f4d2b5e3555ea2ab4275fd72cfda7f78a3371
Dinesh Umasankar <dqu5021@psu.edu>
2023-10-04 09:44:19 -0400

    fix: Followed Clerk Team's SignIn/SignUp Advice

--- src/app/(auth)/(routes)/sign-in/[[...sign-in]]/page.tsx
+++ src/app/(auth)/(routes)/sign-in/[[...sign-in]]/page.tsx
@@ -2,9 +2,6 @@ import { SignIn } from "@clerk/nextjs";
 
 export default function Page() {
   return (
-    <SignIn
-      afterSignInUrl={process.env.NEXT_PUBLIC_AFTER_SIGN_IN_URL}
-      signUpUrl={process.env.NEXT_PUBLIC_CLERK_SIGN_UP_URL}
-    />
+    <SignIn />
   )
 }
--- src/app/(auth)/(routes)/sign-up/[[...sign-up]]/page.tsx
+++ src/app/(auth)/(routes)/sign-up/[[...sign-up]]/page.tsx
@@ -2,9 +2,6 @@ import { SignUp } from "@clerk/nextjs";
 
 export default function Page() {
   return (
-    <SignUp
-      afterSignUpUrl={process.env.NEXT_PUBLIC_AFTER_SIGN_UP_URL}
-      signInUrl={process.env.NEXT_PUBLIC_CLERK_SIGN_IN_URL}
-    />
+    <SignUp />
   )
 }
