8f90023cac4dc1abc5e8091a95626ffaf3f6dc8c
John Nguyen <johnnguyen4501@gmail.com>
2023-10-21 23:42:26 -0400

    more eslint changes

--- src/app/_components/user-auth-form.tsx
+++ src/app/_components/user-auth-form.tsx
@@ -5,63 +5,74 @@ import { Input } from "@/app/_components/ui/input";
 import { Button } from "@/app/_components/ui/button";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/app/_components/ui/form";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/app/_components/ui/form";
 import { Icons } from "@/app/_components/ui/icons";
 import { toast } from "@/app/_components/ui/use-toast";
-import { TUserAuthForm, UserAuthFormSchema } from "@/app/_lib/zod-schemas/user-auth";
+import {
+  TUserAuthForm,
+  UserAuthFormSchema,
+} from "@/app/_lib/zod-schemas/user-auth";
 import { trpc } from "@/app/_trpc/react";
 import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
 
-
-interface UserAuthFormProps extends React.HTMLAttributes<HTMLDivElement> { }
+type UserAuthFormProps = React.HTMLAttributes<HTMLDivElement>;
 
 export function UserAuthForm({ className, ...props }: UserAuthFormProps) {
-
   // Handle stale magic link error from /api/auth/callback redirect
   // by displaying an error toast if there is an `?error=` as part of the url
   toastErrorParams();
 
   async function handleSignInWithGoogle() {
-    const baseURL = (typeof window !== "undefined") ? window.location.origin : '';
+    const baseURL = typeof window !== "undefined" ? window.location.origin : "";
 
     const supabase = createClientComponentClient();
     await supabase.auth.signInWithOAuth({
-      provider: 'google',
+      provider: "google",
       options: {
-        redirectTo: `${baseURL}/api/auth/callback`
-      }
-    })
+        redirectTo: `${baseURL}/api/auth/callback`,
+      },
+    });
   }
 
   // Form Definition
   const form = useForm<TUserAuthForm>({
-    resolver: zodResolver(UserAuthFormSchema)
-  })
+    resolver: zodResolver(UserAuthFormSchema),
+  });
 
   const emailLoginMutation = trpc.auth.email_login.useMutation({
+    // eslint-disable-next-line @typescript-eslint/require-await
     onSuccess: async () => {
       toast({
         variant: "success",
         title: "Login Magic Link Sent!",
-        description: "Check your email for a secure link to effortlessly log in. Time-sensitive for your security.",
-        duration: 2000
+        description:
+          "Check your email for a secure link to effortlessly log in. Time-sensitive for your security.",
+        duration: 2000,
       });
     },
     onError: () => {
       toast({
         variant: "destructive",
         title: "Oops, Something Went Wrong!",
-        description: "If you've encountered an issue, please contact our event administrators for assistance. We apologize for any inconvenience and will resolve it promptly.",
-        duration: 2000
-      })
-    }
+        description:
+          "If you've encountered an issue, please contact our event administrators for assistance. We apologize for any inconvenience and will resolve it promptly.",
+        duration: 2000,
+      });
+    },
   });
 
   async function onSubmit(values: TUserAuthForm) {
     try {
-      await emailLoginMutation.mutateAsync({ email: values.email })
+      await emailLoginMutation.mutateAsync({ email: values.email });
       form.reset();
-    } catch (err: any) {
+    } catch (err: unknown) {
       console.log(err);
     }
   }
@@ -69,7 +80,11 @@ export function UserAuthForm({ className, ...props }: UserAuthFormProps) {
   return (
     <div className={cn("grid gap-6", className)} {...props}>
       <Form {...form}>
-        <form id="userAuthForm" onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+        <form
+          id="userAuthForm"
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="space-y-4"
+        >
           <FormField
             control={form.control}
             name="email"
@@ -77,15 +92,25 @@ export function UserAuthForm({ className, ...props }: UserAuthFormProps) {
               <FormItem>
                 <FormLabel>Email</FormLabel>
                 <FormControl>
-                  <Input className="border-muted-foreground" id="email" placeholder="yourname@example.com" {...field} value={field.value ?? ''} />
+                  <Input
+                    className="border-muted-foreground"
+                    id="email"
+                    placeholder="yourname@example.com"
+                    {...field}
+                    value={field.value ?? ""}
+                  />
                 </FormControl>
                 <FormMessage />
               </FormItem>
             )}
           />
-          <Button type="submit" className="w-full" disabled={form.formState.isSubmitting}>
+          <Button
+            type="submit"
+            className="w-full"
+            disabled={form.formState.isSubmitting}
+          >
             {form.formState.isSubmitting && (
-              <Icons.spinner className="w-4 h-4 mr-2 animate-spin" />
+              <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
             )}
             Sign In with Email
           </Button>
@@ -95,22 +120,28 @@ export function UserAuthForm({ className, ...props }: UserAuthFormProps) {
               <span className="w-full border-t" />
             </div>
             <div className="relative flex justify-center text-xs uppercase">
-              <span className="px-2 bg-background text-muted-foreground">
+              <span className="bg-background px-2 text-muted-foreground">
                 Or continue with
               </span>
             </div>
           </div>
 
-          <Button variant="outline" onClick={handleSignInWithGoogle} className="w-full border-muted-foreground" type="button" disabled={form.formState.isSubmitting}>
+          <Button
+            variant="outline"
+            onClick={handleSignInWithGoogle}
+            className="w-full border-muted-foreground"
+            type="button"
+            disabled={form.formState.isSubmitting}
+          >
             {form.formState.isSubmitting ? (
-              <Icons.spinner className="w-4 h-4 mr-2 animate-spin" />
+              <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
             ) : (
-              <Icons.google className="w-4 h-4 mr-2" />
+              <Icons.google className="mr-2 h-4 w-4" />
             )}{" "}
             Google
           </Button>
         </form>
       </Form>
-    </div >
-  )
+    </div>
+  );
 }
