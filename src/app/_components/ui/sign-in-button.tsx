24bf47d2d34817cd6e50cf2e1f05f9bf013868b9
Dinesh Umasankar <dqu5021@psu.edu>
2023-10-23 13:24:34 -0400

    Sign In Redirect Button

--- src/app/_components/ui/sign-in-button.tsx
+++ src/app/_components/ui/sign-in-button.tsx
@@ -3,14 +3,17 @@
 import { ArrowRight } from "lucide-react";
 import { Button } from "./button";
 import { cn } from "@/app/_lib/client-utils";
+import Link from "next/link";
 
 type SignInButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement>;
 
 export function SignInButton({ className }: SignInButtonProps) {
   return (
-    <Button className={cn("h-8 mr-4 font-semibold", className)}>
-      <span>Login</span>
-      <ArrowRight className="w-4 h-4 ml-2" />
+    <Button className={cn("h-8 mr-4 font-semibold", className)} asChild>
+      <Link href="/sign-in">
+        <span>Login</span>
+        <ArrowRight className="w-4 h-4 ml-2" />
+      </Link>
     </Button>
   )
 }
