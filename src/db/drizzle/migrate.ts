42306788bb588702e19fef8b86d5106d6ebcef0a
John Nguyen <johnnguyen4501@gmail.com>
2023-10-23 21:41:54 -0400

    add script comment

--- src/db/drizzle/migrate.ts
+++ src/db/drizzle/migrate.ts
@@ -1,6 +1,7 @@
 import { migrate } from "drizzle-orm/postgres-js/migrator";
 import { db } from "./index";
 
+//run this script with >ts-node src/db/drizzle/migrate.ts
 async function runMigration() {
   await migrate(db, { migrationsFolder: "src/db/drizzle" });
   console.log("migration is a success");
