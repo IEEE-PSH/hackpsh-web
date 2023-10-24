6dc3032a33d071709c9a13ea555abed62601e106
John Nguyen <johnnguyen4501@gmail.com>
2023-10-24 19:35:51 -0400

    organized headers

--- src/app/(main)/(routes)/dashboard/page.tsx
+++ src/app/(main)/(routes)/dashboard/page.tsx
@@ -1,21 +1,8 @@
-import { SiteHeader } from "@/app/_components/nav/site-header";
-import { ProtectedMainNav } from "@/app/_components/nav/protected-main-nav";
-import { ProtectedMobileNav } from "@/app/_components/nav/protected-mobile-nav";
-import { SiteHeaderActions } from "@/app/_components/nav/site-header-actions";
-import SignOutButton from "@/app/_components/ui/sign-out-button";
-import ProfileButton from "@/app/_components/nav/profile-button";
-
+import ProtectedSiteHeader from "@/app/_components/nav/protected-site-header";
 export default function Page() {
   return (
     <div>
-      <SiteHeader>
-        <ProtectedMainNav />
-        <ProtectedMobileNav />
-        <SiteHeaderActions>
-          <ProfileButton />
-        </SiteHeaderActions>
-      </SiteHeader>
-      <SignOutButton />
+      <ProtectedSiteHeader />
     </div>
   );
 }
