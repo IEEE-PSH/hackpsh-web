3b0d2ee7162ab3c27408c1cb2cd934dfef0307da
Dinesh Umasankar <dqu5021@psu.edu>
2023-10-24 20:05:37 -0400

    Migrate Script via Esno & Lint Fix

--- src/db/drizzle/index.ts
+++ src/db/drizzle/index.ts
@@ -1,12 +1,8 @@
 import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
 import * as schema from "./schema";
-import dotenv from "dotenv";
 
-//must load dotenv to allow ts-node to access env vars
-dotenv.config();
-//added "||" because eslint flags otherwise
-const connectionString = process.env.DATABASE_URL || "";
+const connectionString = process.env.DATABASE_URL;
 const client = postgres(connectionString);
 
 export const db = drizzle(client, { schema: schema });
--- src/db/drizzle/migrate.ts
+++ src/db/drizzle/migrate.ts
@@ -1,12 +1,19 @@
 import { migrate } from "drizzle-orm/postgres-js/migrator";
-import { db } from "./index";
+import { drizzle } from "drizzle-orm/postgres-js";
+import postgres from "postgres";
+import * as schema from "./schema";
+import dotenv from "dotenv";
 
-//run this script with >ts-node src/db/drizzle/migrate.ts
-async function runMigration() {
-  await migrate(db, { migrationsFolder: "src/db/drizzle" });
-  console.log("migration is a success");
-}
+dotenv.config();
 
-runMigration().catch((err) => {
-  console.log(err);
-});
+const connectionString = process.env.DATABASE_URL;
+const client = postgres(connectionString);
+
+const db = drizzle(client, { schema: schema });
+
+migrate(db, { migrationsFolder: "src/db/drizzle" })
+  .then(() => console.log("migration success"))
+  .catch(async (err) => {
+    console.log(err);
+    await client.end({ timeout: 5000 });
+  });
