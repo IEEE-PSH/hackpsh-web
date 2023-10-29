4722688d54fbffcc19338f51dc6e3e09e7f17eb9
John Nguyen <69529980+ImJunny@users.noreply.github.com>
2023-10-29 17:17:58 -0400

    Added Basic Header Titles to Most Pages (#27)
    * Add titles to access those pages
    
    * Add dashboard title
    
    ---------
    
    Co-authored-by: Dinesh Umasankar <dqu5021@psu.edu>

---
+++ src/app/(main)/(routes)/partners/page.tsx
@@ -0,0 +1,11 @@
+import PublicSiteHeader from "@/app/_components/nav/public-site-header";
+
+export default function Page() {
+  return (
+    <div>
+      <PublicSiteHeader />
+
+      <p className="m-10 text-center text-3xl font-bold">Partners</p>
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
 import PublicSiteHeader from "./_components/nav/public-site-header";
 
 export default function Page() {
-  return <PublicSiteHeader />;
+  return (
+    <div>
+      <PublicSiteHeader />
+
+      <p className="m-10 text-center text-3xl font-bold">Home</p>
+    </div>
+  );
 }
