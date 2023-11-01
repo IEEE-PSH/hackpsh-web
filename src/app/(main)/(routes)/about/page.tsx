83efbae79d7ee00c7c4ddcca2741446bd2f480c3
Dinesh Umasankar <dqu5021@psu.edu>
2023-11-01 10:26:29 -0400

    More Metadata Updates

--- src/app/(main)/(routes)/about/page.tsx
+++ src/app/(main)/(routes)/about/page.tsx
@@ -1,10 +1,16 @@
 import PublicSiteHeader from "@/app/_components/nav/public-site-header";
+import { type Metadata } from "next";
 
-export default function Page() {
+export const metadata: Metadata = {
+  title: "About | HackPSH",
+  description: "Learn what HackPSH Is about!",
+}
+
+export default function AboutPage() {
   return (
     <div>
       <PublicSiteHeader />
-      <p className="m-10 text-center text-3xl font-bold">About</p>
+      <p className="m-10 text-3xl font-bold text-center">About</p>
     </div>
   );
 }
--- src/app/(main)/(routes)/challenges/page.tsx
+++ src/app/(main)/(routes)/challenges/page.tsx
@@ -1,11 +1,17 @@
 import React from "react";
 import ProtectedSiteHeader from "@/app/_components/nav/protected-site-header";
+import { type Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Challenges | HackPSH",
+  description: "Pick a challenge to solve to gain points!",
+}
 
 export default function ChallengesPage() {
   return (
     <div>
       <ProtectedSiteHeader />
-      <p className="m-10 text-center text-3xl font-bold">Challenges</p>
+      <p className="m-10 text-3xl font-bold text-center">Challenges</p>
     </div>
   );
 }
