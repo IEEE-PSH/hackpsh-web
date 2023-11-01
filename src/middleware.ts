d90ca26cfe567e7977141c96186a8a7d6f7bd1b1
Dinesh Umasankar <dqu5021@psu.edu>
2023-10-31 23:28:43 -0400

    Adjust middleware for edge runtime
    Next.js middleware is forced to run on edge run-time which doesn't support the net module (dependency of node-postgres) within the original implementation of tRPC server-side caller.

--- src/middleware.ts
+++ src/middleware.ts
@@ -9,10 +9,8 @@ import {
   composeMiddlewareClient,
   getSession,
 } from "./app/_lib/supabase/server";
-import { userRouter } from "./server/routers/user";
-import { db } from "./db/drizzle";
-import { TRPCError } from "@trpc/server";
 import handleError from "./app/_lib/server/handleError";
+import { serverTRPC } from "./app/_trpc/server";
 
 export async function middleware(req: NextRequest) {
   try {
@@ -20,20 +18,18 @@ export async function middleware(req: NextRequest) {
     const supabase = composeMiddlewareClient(req, res);
     const session = await getSession(supabase);
 
-    // Create Server-Side Caller `User` to access Procedures
-    const userRPC = userRouter.createCaller({
-      db,
-      req,
-      supabase,
-      headers: req.headers,
-      session: session,
-    });
-
     // If the user has not completed onboarding, then
     // redirect the user to the onboarding forms.
-    const { is_onboarding_complete } = await userRPC.is_onboarding_complete({
-      user_uuid: session.user.id,
-    });
+
+    // Forced to use serverTRPC in middleware due to
+    // Next.js Middleware forced on Edge Runtime,
+    // which doesn't support the net module
+    // (dep of node-postgres / drizzle) when
+    // using server-side callers (like /api/auth/callback)
+    const { is_onboarding_complete } =
+      await serverTRPC.user.is_onboarding_complete.query({
+        user_uuid: session.user.id,
+      });
 
     if (
       !is_onboarding_complete &&
