313c3b1d6d142483e86190a45f78774d7ac6137a
Dinesh Umasankar <dqu5021@psu.edu>
2023-11-03 16:13:30 -0400

    Personal Details TRPC Route

--- src/server/procedures/protected/onboarding/updatePersonalDetailsProcedure.ts
+++ src/server/procedures/protected/onboarding/updatePersonalDetailsProcedure.ts
@@ -1,5 +1,19 @@
+import { updateUserPersonalDetails } from "@/server/dao/onboarding";
 import { protectedProcedure } from "@/server/trpc";
+import { UpdateUserPersonalDetailsSchema } from "@/server/zod-schemas/onboarding";
 
-export default protectedProcedure.mutation(() => {
-  return true;
-});
+export default protectedProcedure
+  .input(UpdateUserPersonalDetailsSchema)
+  .mutation(async ({ ctx, input }) => {
+    await updateUserPersonalDetails(
+      ctx.db,
+      input.user_uuid,
+      input.user_display_name,
+      input.user_school_year,
+      input.user_major,
+    );
+
+    return {
+      update_user_personal_details: true,
+    };
+  });
