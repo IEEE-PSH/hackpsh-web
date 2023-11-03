8c7e96e993400cb619b453da8dd930444dac9371
John Nguyen <69529980+ImJunny@users.noreply.github.com>
2023-11-03 12:11:58 -0400

    Add Major Table and Support to Profile Table (#44)
    * first commit
    
    * Enable Row Level Security
    
    ---------
    
    Co-authored-by: Dinesh Umasankar <dqu5021@psu.edu>

--- src/db/drizzle/migrate.ts
+++ src/db/drizzle/migrate.ts
@@ -63,6 +63,181 @@ migrate(db, { migrationsFolder: "src/db/drizzle" })
       .values({ school_year_name: "post_graduate" })
       .onConflictDoNothing();
 
+    await db
+      .insert(schema.app_major)
+      .values({ major_name: "non_applicable" })
+      .onConflictDoNothing();
+    await db
+      .insert(schema.app_major)
+      .values({ major_name: "undecided" })
+      .onConflictDoNothing();
+    await db
+      .insert(schema.app_major)
+      .values({ major_name: "accounting" })
+      .onConflictDoNothing();
+    await db
+      .insert(schema.app_major)
+      .values({ major_name: "american_studies" })
+      .onConflictDoNothing();
+    await db
+      .insert(schema.app_major)
+      .values({ major_name: "biobehavioral_health" })
+      .onConflictDoNothing();
+    await db
+      .insert(schema.app_major)
+      .values({ major_name: "biology" })
+      .onConflictDoNothing();
+    await db
+      .insert(schema.app_major)
+      .values({ major_name: "civil_engineering" })
+      .onConflictDoNothing();
+    await db
+      .insert(schema.app_major)
+      .values({ major_name: "communication_sciences_and_disorders" })
+      .onConflictDoNothing();
+    await db
+      .insert(schema.app_major)
+      .values({ major_name: "communications" })
+      .onConflictDoNothing();
+    await db
+      .insert(schema.app_major)
+      .values({ major_name: "computer_science" })
+      .onConflictDoNothing();
+    await db
+      .insert(schema.app_major)
+      .values({ major_name: "criminal_justice" })
+      .onConflictDoNothing();
+    await db
+      .insert(schema.app_major)
+      .values({ major_name: "cybersecurity_analytics_and_operations" })
+      .onConflictDoNothing();
+    await db
+      .insert(schema.app_major)
+      .values({ major_name: "electrical_engineering" })
+      .onConflictDoNothing();
+    await db
+      .insert(schema.app_major)
+      .values({ major_name: "electrical_engineering_technology" })
+      .onConflictDoNothing();
+    await db
+      .insert(schema.app_major)
+      .values({ major_name: "elementary_education" })
+      .onConflictDoNothing();
+    await db
+      .insert(schema.app_major)
+      .values({ major_name: "english" })
+      .onConflictDoNothing();
+    await db
+      .insert(schema.app_major)
+      .values({ major_name: "enterprise_technology_integration" })
+      .onConflictDoNothing();
+    await db
+      .insert(schema.app_major)
+      .values({ major_name: "finance" })
+      .onConflictDoNothing();
+    await db
+      .insert(schema.app_major)
+      .values({ major_name: "health_policy_and_administration" })
+      .onConflictDoNothing();
+    await db
+      .insert(schema.app_major)
+      .values({ major_name: "human_capital_management" })
+      .onConflictDoNothing();
+    await db
+      .insert(schema.app_major)
+      .values({ major_name: "human_development_and_family_studies" })
+      .onConflictDoNothing();
+    await db
+      .insert(schema.app_major)
+      .values({ major_name: "human_centered_design_and_development" })
+      .onConflictDoNothing();
+    await db
+      .insert(schema.app_major)
+      .values({ major_name: "humanities" })
+      .onConflictDoNothing();
+    await db
+      .insert(schema.app_major)
+      .values({ major_name: "information_sciences_and_technology" })
+      .onConflictDoNothing();
+    await db
+      .insert(schema.app_major)
+      .values({ major_name: "information_systems" })
+      .onConflictDoNothing();
+    await db
+      .insert(schema.app_major)
+      .values({ major_name: "kinesiology" })
+      .onConflictDoNothing();
+    await db
+      .insert(schema.app_major)
+      .values({ major_name: "management" })
+      .onConflictDoNothing();
+    await db
+      .insert(schema.app_major)
+      .values({ major_name: "marketing" })
+      .onConflictDoNothing();
+    await db
+      .insert(schema.app_major)
+      .values({ major_name: "mathematical_sciences" })
+      .onConflictDoNothing();
+    await db
+      .insert(schema.app_major)
+      .values({ major_name: "mechanical_engineering" })
+      .onConflictDoNothing();
+    await db
+      .insert(schema.app_major)
+      .values({ major_name: "mechanical_engineering_technology" })
+      .onConflictDoNothing();
+    await db
+      .insert(schema.app_major)
+      .values({ major_name: "nursing" })
+      .onConflictDoNothing();
+    await db
+      .insert(schema.app_major)
+      .values({ major_name: "political_science" })
+      .onConflictDoNothing();
+    await db
+      .insert(schema.app_major)
+      .values({ major_name: "project_and_supply_chain_management" })
+      .onConflictDoNothing();
+    await db
+      .insert(schema.app_major)
+      .values({ major_name: "psychology" })
+      .onConflictDoNothing();
+    await db
+      .insert(schema.app_major)
+      .values({ major_name: "public_policy" })
+      .onConflictDoNothing();
+    await db
+      .insert(schema.app_major)
+      .values({ major_name: "science" })
+      .onConflictDoNothing();
+    await db
+      .insert(schema.app_major)
+      .values({ major_name: "secondary_education_english" })
+      .onConflictDoNothing();
+    await db
+      .insert(schema.app_major)
+      .values({ major_name: "secondary_education_mathematics" })
+      .onConflictDoNothing();
+    await db
+      .insert(schema.app_major)
+      .values({ major_name: "secondary_education_social_studies" })
+      .onConflictDoNothing();
+    await db
+      .insert(schema.app_major)
+      .values({ major_name: "security_and_risk_analysis" })
+      .onConflictDoNothing();
+    await db
+      .insert(schema.app_major)
+      .values({ major_name: "sociology" })
+      .onConflictDoNothing();
+    await db
+      .insert(schema.app_major)
+      .values({
+        major_name: "structural_design_and_construction_engineering_technology",
+      })
+      .onConflictDoNothing();
+
     await pool.end();
   })
   .catch(async (err) => {
