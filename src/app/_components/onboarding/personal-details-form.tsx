d2b1a4d5f19e07451800f0d0091555a3e580c735
Dinesh Umasankar <dqu5021@psu.edu>
2023-11-04 00:43:22 -0400

    Front-End UI Personal Details

--- src/app/_components/onboarding/personal-details-form.tsx
+++ src/app/_components/onboarding/personal-details-form.tsx
@@ -8,6 +8,11 @@ import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, For
 import { Icons } from "../ui/icons";
 import { Button } from "../ui/button";
 import { Input } from "../ui/input";
+import { dbMajors, dbSchoolYear } from "@/db/drizzle/startup_seed";
+import { Command, CommandEmpty, CommandGroup, CommandInput, CommandItem } from "../ui/command";
+import { Popover, PopoverContent, PopoverTrigger } from "../ui/combo-scroll-popover";
+import { Check, ChevronsUpDown } from "lucide-react";
+import { ScrollArea } from "../ui/scroll-area";
 
 type OnboardingPersonalDetailsFormProps = React.HTMLAttributes<HTMLDivElement>;
 
@@ -29,7 +34,7 @@ export default function OnboardingPersonalDetailsForm({ className, ...props }: O
         <form
           id="onboardingPersonalDetailsForm"
           onSubmit={form.handleSubmit(onSubmit)}
-          className="space-y-6"
+          className="space-y-8"
         >
           <FormField
             control={form.control}
@@ -52,6 +57,128 @@ export default function OnboardingPersonalDetailsForm({ className, ...props }: O
               </FormItem>
             )}
           />
+
+          <FormField
+            control={form.control}
+            name="user_school_year"
+            render={({ field }) => (
+              <FormItem className="flex flex-col">
+                <FormLabel>School Year</FormLabel>
+                <Popover>
+                  <PopoverTrigger asChild>
+                    <FormControl>
+                      <Button
+                        variant={"outline"}
+                        role="combobox"
+                        className={cn("justify-between", !field.value && "text-muted-foreground")}
+                      >
+                        {field.value
+                          ? dbSchoolYear.find(
+                            (school_year) => school_year === field.value
+                          )?.replace("_", " ").toLocaleUpperCase()
+                          : "Select school year"}
+                        <ChevronsUpDown className="w-4 h-4 ml-2 opacity-50 shrink-0" />
+                      </Button>
+                    </FormControl>
+                  </PopoverTrigger>
+                  <PopoverContent className="p-0">
+                    <Command>
+                      <CommandInput placeholder="Search school year..." />
+                      <CommandEmpty>Cannot find provided school year.</CommandEmpty>
+                      <CommandGroup>
+                        {dbSchoolYear.map((school_year) => (
+                          <CommandItem
+                            value={school_year}
+                            key={school_year}
+                            onSelect={() => {
+                              form.setValue("user_school_year", school_year)
+                            }}
+                          >
+                            <Check
+                              className={cn(
+                                "mr-2 h-4 w-4",
+                                school_year === field.value
+                                  ? "opacity-100"
+                                  : "opacity-0"
+                              )}
+                            />
+                            {school_year.replace("_", " ").toLocaleUpperCase()}
+                          </CommandItem>
+                        ))}
+                      </CommandGroup>
+                    </Command>
+                  </PopoverContent>
+                </Popover>
+                <FormDescription>
+                  Select a school year closest to your current academic status.
+                </FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name="user_major"
+            render={({ field }) => (
+              <FormItem className="flex flex-col">
+                <FormLabel>Major</FormLabel>
+                <Popover>
+                  <PopoverTrigger asChild>
+                    <FormControl>
+                      <Button
+                        variant={"outline"}
+                        role="combobox"
+                        className={cn("justify-between", !field.value && "text-muted-foreground")}
+                      >
+                        {field.value
+                          ? dbMajors.find(
+                            (major_name) => major_name === field.value
+                          )?.replaceAll("_", " ").toLocaleUpperCase()
+                          : "Select major"}
+                        <ChevronsUpDown className="w-4 h-4 ml-2 opacity-50 shrink-0" />
+                      </Button>
+                    </FormControl>
+                  </PopoverTrigger>
+                  <PopoverContent className="h-[350px] w-[400px] p-0">
+                    <Command>
+                      <CommandInput placeholder="Search majors..." />
+                      <ScrollArea className="h-[350px]">
+
+                        <CommandEmpty>Cannot find provided major.</CommandEmpty>
+                        <CommandGroup>
+                          {dbMajors.map((major_name) => (
+                            <CommandItem
+                              value={major_name}
+                              key={major_name}
+                              onSelect={() => {
+                                form.setValue("user_major", major_name)
+                              }}
+                            >
+                              <Check
+                                className={cn(
+                                  "mr-2 h-4 w-4",
+                                  major_name === field.value
+                                    ? "opacity-100"
+                                    : "opacity-0"
+                                )}
+                              />
+                              {major_name.replaceAll("_", " ").toLocaleUpperCase()}
+                            </CommandItem>
+                          ))}
+                        </CommandGroup>
+                      </ScrollArea>
+                    </Command>
+                  </PopoverContent>
+                </Popover>
+                <FormDescription>
+                  Select a major that you are pursuing or the most interested in.
+                </FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
           <Button type="submit" className="w-full" disabled={form.formState.isSubmitting}>
             {form.formState.isSubmitting && (
               <Icons.spinner className="w-4 h-4 mr-2 animate-spin" />
@@ -190,16 +317,6 @@ export default function OnboardingPersonalDetailsForm({ className, ...props }: O
 //               Back
 //             </Link>
 
-//             <Button
-//               type="submit"
-//               className="w-full "
-//               disabled={form.formState.isSubmitting}
-//             >
-//               {form.formState.isSubmitting && (
-//                 <Icons.spinner className="w-4 h-4 mr-2 animate-spin" />
-//               )}
-//               Next
-//             </Button>
 //           </div>
 //         </form>
 //       </Form>
