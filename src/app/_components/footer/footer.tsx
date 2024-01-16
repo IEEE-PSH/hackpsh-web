e51822612d96e2088b40c7546d76707e7e6fac7a
John Nguyen <69529980+ImJunny@users.noreply.github.com>
2024-01-16 21:27:22 +0000

    fix footer

--- src/app/_components/footer/footer.tsx
+++ src/app/_components/footer/footer.tsx
@@ -7,52 +7,52 @@ import { Icons } from "../ui/icons";
 export default function Footer() {
   return (
     <div className="mt-auto">
-    <div className="bottom-0 w-full absolute py-16 bg-background">
-      <div className="flex flex-row items-start container max-w-[64rem] justify-center text-foreground lg:justify-between">
-        <Link
-          href={siteConfig.paths.home}
-          className="mr-auto hidden origin-top-left scale-[2] items-center space-x-2 lg:flex"
-          scroll={false}
-        >
-          <Icons.brand className="h-[2.4rem] w-[2.0rem]" />
-          <span className="hidden text-xl font-bold sm:inline-block">
-            {siteConfig.name}
-          </span>
-        </Link>
-        <div className="flex flex-row space-x-20">
-          <div>
-            <p className="text-lg font-bold">Follow Us</p>
-            <ul className="text-lg">
-              <li>LinkedIn</li>
-              <li>Facebook</li>
-              <li>Instagram</li>
-            </ul>
-          </div>
-          <div>
-            <p className="text-lg font-bold">Directory</p>
-            <ul className="text-lg">
-              <li>Home</li>
-              <li>About</li>
-              <li>Partners</li>
-            </ul>
-          </div>
-          <div>
-            <p className="text-lg font-bold">Information</p>
-            <ul className="text-lg">
-              <li>Contact</li>
-              <li>Privacy Policy</li>
-              <li>Terms and Service</li>
-            </ul>
-          </div>
-          <div className="hidden md:block">
-            <p className="text-lg font-bold">Other Links</p>
-            <ul>
-              <li>PSH IEEE</li>
-            </ul>
+      <div className="absolute bottom-0 w-full bg-background py-16">
+        <div className="container flex max-w-[64rem] flex-row items-start justify-center text-foreground lg:justify-between">
+          <Link
+            href={siteConfig.paths.home}
+            className="mr-auto hidden origin-top-left scale-[2] items-center space-x-2 lg:flex"
+            scroll={false}
+          >
+            <Icons.brand className="h-[2.4rem] w-[2.0rem]" />
+            <span className="hidden text-xl font-bold sm:inline-block">
+              {siteConfig.name}
+            </span>
+          </Link>
+          <div className="flex flex-row space-x-16 md:space-x-20">
+            <div>
+              <p className="text-lg font-bold">Follow Us</p>
+              <ul className="text-lg">
+                <li>LinkedIn</li>
+                <li>Facebook</li>
+                <li>Instagram</li>
+              </ul>
+            </div>
+            <div>
+              <p className="text-lg font-bold">Directory</p>
+              <ul className="text-lg">
+                <li>Home</li>
+                <li>About</li>
+                <li>Partners</li>
+              </ul>
+            </div>
+            <div>
+              <p className="text-lg font-bold">Information</p>
+              <ul className="text-lg">
+                <li>Contact</li>
+                <li>Privacy Policy</li>
+                <li>Terms and Service</li>
+              </ul>
+            </div>
+            <div className="hidden md:block">
+              <p className="text-lg font-bold">Other Links</p>
+              <ul>
+                <li>PSH IEEE</li>
+              </ul>
+            </div>
           </div>
         </div>
       </div>
-    </div>
     </div>
   );
 }
