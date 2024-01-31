2d70ff4a9d829deb033a4071d3a7a147574c63c4
Dinesh Umasankar <dqu5021@psu.edu>
2024-01-31 10:22:33 -0500

    Finalized Implementation of Feedback Page

--- src/app/(main)/(routes)/feedback/page.tsx
+++ src/app/(main)/(routes)/feedback/page.tsx
@@ -9,6 +9,7 @@ import { Form, FormLabel, FormField, FormItem, FormControl, FormMessage } from "
 import { Input } from "@/app/_components/ui/input";
 import { Textarea } from "@/app/_components/ui/textarea";
 import { toast } from "@/app/_components/ui/use-toast";
+import { Icons } from "@/app/_components/ui/icons";
 import { cn } from "@/app/_lib/client-utils";
 
 export default function FeedbackPage() {
@@ -22,39 +23,97 @@ export default function FeedbackPage() {
   );
 }
 
-// Goal: Build out the Create FeedbackReportForm which has a input for name and
-// a textbox for the feedback report.
-//
-// Hint: Look closesly at `src/app/(main)/(routes)/announcements/create-post/page.tsx`
-//
-// Goal A: Create the Form Schema (Client-side Form Validation)
-// This is a zod object defining the shape of data that your form will accept
-// - feedback_name string [min 1 char] ("The name of this report cannot be empty")
-// - feedback_report string [min 1 char] ("The report's contents may not be empty")
-//
 const CreateFeedbackReportFormSchema = z.object({
-
+  feedback_name: z.string().min(1, "The name of this report cannot be empty."),
+  feedback_report: z.string().min(1, "The report's contents cannot be empty."),
 })
 
 type TCreateFeedbackForm = z.infer<typeof CreateFeedbackReportFormSchema>;
 type CreateAnnouncementFormProps = React.HTMLAttributes<HTMLDivElement>;
 
-// Goal B: Create a Form Object and define what shape of data it can accept, and its own validation rules
-// Goal C: Utilize trpc to invoke the mutation and handle the success and error callback from data submission
-// Goal D: Create an onSubmit handler to simply invoke the mutation and reset the form's values within a try-catch
-// Goal E: Utilize shadCN to build out the HTML to display an input field and textarea
-function CreateFeedbackReportForm({ className, ...props }: CreateAnnouncementFormProps) {
+function CreateFeedbackReportForm({className, ...props }: CreateAnnouncementFormProps) {
   const form = useForm<TCreateFeedbackForm>({
-
+    resolver: zodResolver(CreateFeedbackReportFormSchema)
   })
 
-  const feedbackMutation = null;
+  const feedbackMutation = trpc.feedback.create_feedback_report.useMutation({
+    onSuccess: () => {
+      toast({
+        variant: "success",
+        title: "Feedback Report Created!",
+        duration: 4000,
+      });
+    },
+    onError: () => {
+      toast({
+        variant: "destructive",
+        title: "Oops, Something Went Wrong!",
+        description:
+          "If you've encountered an issue, please contact our event administrators for assistance. We apologize for any inconvenience and will resolve it promptly.",
+        duration: 6000,
+      });
+    },
+  });
 
-  async function onSubmit(values: TCreateFeedbackForm) { }
+  async function onSubmit(values: TCreateFeedbackForm) {
+    try {
+      await feedbackMutation.mutateAsync({
+        feedback_name: values.feedback_name,
+        feedback_report: values.feedback_report
+      });
+      form.reset();
+    } catch (err) {
+      console.log(err);
+    }
+  }
 
   return (
     <>
       <div className={cn("grid-gap-6", className)} {...props}>
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="w-full space-y-6"
+          >
+            <FormField
+              control={form.control}
+              name="feedback_name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Feedback Title</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Report Name" {...field} value={field.value ?? ""} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="feedback_report"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Feedback Report</FormLabel>
+                  <FormControl>
+                    <Textarea
+                      placeholder="Give your feedback here!"
+                      className="resize-none h-36"
+                      {...field}
+                      value={field.value ?? ""}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button type="submit" className="w-full" disabled={form.formState.isSubmitting}>
+              {form.formState.isSubmitting && (
+                <Icons.spinner className="w-4 h-4 mr-2 animate-spin" />
+              )}
+              Submit
+            </Button>
+          </form>
+        </Form>
       </div>
     </>
   );
