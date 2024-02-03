4075d1cc7d95842fca99293c9d4dd6b6e25c731f
John Nguyen <69529980+ImJunny@users.noreply.github.com>
2024-02-03 00:53:30 +0000

    make mobile footer

--- src/app/(main)/(routes)/(public)/layout.tsx
+++ src/app/(main)/(routes)/(public)/layout.tsx
@@ -1,17 +1,11 @@
 import Footer from "@/app/_components/footer/footer";
 import PublicSiteHeader from "@/app/_components/nav/public-site-header";
-export default function Layout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-      <div className="min-h-screen flex flex-col relative">
-        <PublicSiteHeader />
-        <div className="pb-[17.25rem]">
-          {children}
-        </div>
-        <Footer />
-      </div>
+    <div className="relative flex min-h-screen flex-col">
+      <PublicSiteHeader />
+      <div className="pb-[26.5rem] sm:pb-[16.25rem]">{children}</div>
+      <Footer />
+    </div>
   );
 }
