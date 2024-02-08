2940817d85477b9e7b6411c2e496480407148a9c
John Nguyen <69529980+ImJunny@users.noreply.github.com>
2024-02-08 04:40:16 +0000

    merge About page manually

--- src/app/(main)/(routes)/(public)/about/page.tsx
+++ src/app/(main)/(routes)/(public)/about/page.tsx
@@ -1,14 +1,80 @@
+import Section from "@/app/_components/page-assets/section";
 import { type Metadata } from "next";
+import ContactSection from "@/app/_components/page-assets/contact-section";
 
 export const metadata: Metadata = {
   title: "About | HackPSH",
   description: "Learn what HackPSH Is about!",
 };
 
+function AboutHero() {
+  return (
+    <Section className="bg-slate-800">
+      <h1 className="text-4xl font-bold">About Us</h1>
+      <p className="mt-6">Lorem ipsum</p>
+    </Section>
+  );
+}
+
+function AboutOfficers() {
+  return (
+    <Section>
+      <h1 className="mt-5 text-center text-4xl font-bold">IEEE Officers</h1>
+      <div className="grid grid-cols-2 gap-10">
+        <div className="text-center">
+          <h1 className="font-bold">John Smith</h1>
+          <p>frjfjrfr</p>
+        </div>
+        <div className="text-center">
+          <h1 className="font-bold">John Smith</h1>
+          <p>frjfjrfr</p>
+        </div>
+        <div className="text-center">
+          <h1 className="font-bold">John Smith</h1>
+          <p>frjfjrfr</p>
+        </div>
+        <div className="text-center">
+          <h1 className="font-bold">John Smith</h1>
+          <p>frjfjrfr</p>
+        </div>
+      </div>
+    </Section>
+  );
+}
+
+function Helpers() {
+  return (
+    <Section className="bg-white">
+      <h1 className="text-center text-4xl font-bold text-black">Helpers</h1>
+      <div className="grid grid-cols-2 gap-10 text-black">
+        <div className="text-center">
+          <h1 className="font-bold">John Smith</h1>
+          <p>frjfjrfr</p>
+        </div>
+        <div className="text-center">
+          <h1 className="font-bold">John Smith</h1>
+          <p>frjfjrfr</p>
+        </div>
+        <div className="text-center">
+          <h1 className="font-bold">John Smith</h1>
+          <p>frjfjrfr</p>
+        </div>
+        <div className="text-center">
+          <h1 className="font-bold">John Smith</h1>
+          <p>frjfjrfr</p>
+        </div>
+      </div>
+    </Section>
+  );
+}
+
 export default function Page() {
   return (
-    <div>
-      <p className="m-10 text-center text-3xl font-bold">About</p>
-    </div>
+    <>
+      <AboutHero />
+      <AboutOfficers />
+      <Helpers />
+      <ContactSection className="bg-black" title="Have Feedback?" />
+    </>
   );
 }
