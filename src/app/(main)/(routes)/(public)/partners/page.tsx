0945bf07823fdd443a5f22deb7abe19b5dca3110
Moses Madavaram <mosesgab@gmail.com>
2024-02-16 04:20:47 +0000

    Made the Partners page responsive

--- src/app/(main)/(routes)/(public)/partners/page.tsx
+++ src/app/(main)/(routes)/(public)/partners/page.tsx
@@ -11,11 +11,11 @@ export const metadata: Metadata = {
 function PartnerHero(){
   return(
     <Section className="bg-neutral-400">
-      <div className="justify-left text-left items-center space-y-8 flex-col text-black ml-6 mt-[-40px]">
-      <p className="text-5xl m-5 font-bold">Partners</p> 
+      <div className="flex flex-col text-black mt-[-40px] sm:mt-[-20px] ml-5 sm:ml-8 md:ml-10 lg:ml-12 xl:ml-16 mb-4">
+      <p className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl font-bold">Partners</p> 
       </div>
-      <div className="flex flex-col max-w-2xl ml-6">  
-        <p className="ml-6">
+      <div className="flex flex-col max-w-2xl ml-5 sm:ml-8 md:ml-10 lg:ml-12 xl:ml-16">  
+        <p className="text-left">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
           eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
           minim veniam.
@@ -30,18 +30,18 @@ function PartnerHero(){
 function PartnerInfo() {
   return (
     <Section className="bg-white">
-      <div className="mx-auto grid gap-y-8 text-black">
-        <div className="grid grid-cols-1 gap-y-8 md:grid-cols-2">
+      <div className="mx-auto grid gap-y-8 text-black px-4 sm:px-6 md:px-8 lg:px-10 xl:px-12">
+        <div className="grid grid-cols-1 gap-y-8 md:grid-cols-2 lg:gap-x-8">
           <img
             alt="penn-state-img"
-            className="hidden md:block rounded-md w-50 h-50"
+            className="md:block rounded-md w-full h-auto object-cover"
             src=
               "https://kubrick.htvapps.com/htv-prod-media.s3.amazonaws.com/images/penn-state-logo-1596642604.jpg?crop=1.00xw:1.00xh;0,0&resize=900:*"
             
           />
 
-          <div className="flex flex-col space-y-8 md:px-8">
-            <p className="text-4xl font-bold">Sponsor Name</p>
+          <div className="flex flex-col space-y-8 px-4 sm:px-6 md:px-8">
+            <p className="text-2xl sm:text-3xl md:text-4xl font-bold">Sponsor Name</p>
             <p>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
               eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
@@ -54,17 +54,17 @@ function PartnerInfo() {
           </div>
         </div>
         <hr />
-        <div className="grid grid-cols-1 gap-y-8 md:grid-cols-2">
+        <div className="grid grid-cols-1 gap-y-8 md:grid-cols-2 lg:gap-x-8">
           <img
             alt="penn-state-img"
-            className="hidden md:block rounded-md w-50 h-50"
+            className="md:block rounded-md w-full h-auto object-cover"
             src=
               "https://kubrick.htvapps.com/htv-prod-media.s3.amazonaws.com/images/penn-state-logo-1596642604.jpg?crop=1.00xw:1.00xh;0,0&resize=900:*"
             
           />
 
-          <div className="flex flex-col space-y-8 md:px-8">
-            <p className="text-4xl font-bold">Sponsor Name</p>
+          <div className="flex flex-col space-y-8 px-4 sm:px-6 md:px-8">
+            <p className="text-2xl sm:text-3xl md:text-4xl font-bold">Sponsor Name</p>
             <p>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
               eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
@@ -85,15 +85,15 @@ function PartnerInfo() {
 function ContactHero() {
   return (
     <Section className="bg-gray-950">
-      <div className="flex min-h-[10rem] justify-center text-center items-center space-y-8 flex-col text-white">
-        <p className="text-4xl font-bold">Interested in Partnering?</p> 
+      <div className="flex min-h-[10rem] justify-center text-center items-center space-y-8 flex-col text-white px-4 sm:px-6 md:px-8 lg:px-10 xl:px-12">
+        <p className="text-3xl sm:text=4xl md:text-5xl lg:text-6xl xl:text-7xl font-bold">Interested in Partnering?</p> 
         <div className="flex flex-col max-w-2xl">  
           <p>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
             eiusmod.
           </p>
         </div>
-        <Button className="text-xl h-12 w-48 bg-white hover:bg-white/80">
+        <Button className="text-lg sm:text-xl h-10 sm:h-12 w-40 sm:w-48 bg-white hover:bg-white/80">
           Contact Us
         </Button>
       </div>
