4159146c3ec7963a0b81f28797a366105623a9bd
John Nguyen <69529980+ImJunny@users.noreply.github.com>
2024-02-19 07:06:17 +0000

    change to accurate time

--- src/server/procedures/public/event-status/getTimeRemainingProcedure.ts
+++ src/server/procedures/public/event-status/getTimeRemainingProcedure.ts
@@ -2,7 +2,7 @@ import { publicProcedure } from "@/server/trpc";
 
 export default publicProcedure.query(() => {
   //passed string should be in UTC; EST+=5 Hours
-  const eventStartTime = new Date("2024-02-18T18:56:00");
+  const eventStartTime = new Date("2024-03-23T15:00:00");
   const timeRemaining = eventStartTime.valueOf() - Date.now();
   return { eventStartTime, timeRemaining };
 });
