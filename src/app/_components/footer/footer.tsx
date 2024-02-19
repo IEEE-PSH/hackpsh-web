3750434a8d9e7a6c7683d6e30f061f42c320368b
John Nguyen <69529980+ImJunny@users.noreply.github.com>
2024-02-19 08:19:05 +0000

    simplify footer

--- src/app/_components/footer/footer.tsx
+++ src/app/_components/footer/footer.tsx
@@ -3,77 +3,10 @@ import Link from "next/link";
 import { siteConfig } from "@/app/_config/site";
 import { Icons } from "../ui/icons";
 
-function MobileFooter() {
+export default function Footer() {
   return (
-    <div className="absolute bottom-0 block w-full bg-background py-16 sm:hidden">
-      <div className="container grid max-w-[80rem] grid-cols-2 grid-rows-2 justify-center gap-y-8 text-foreground">
-        <ul className="space-y-3">
-          <p className=" font-bold">Follow Us</p>
-          <li className="hover:underline">
-            <a
-              href="https://www.linkedin.com/company/ieee-penn-state-harrisbrurg/"
-              target="_blank"
-            >
-              LinkedIn
-            </a>
-          </li>
-          <li className="hover:underline">
-            <a
-              href="https://www.facebook.com/groups/183119638427039/"
-              target="_blank"
-            >
-              Facebook
-            </a>
-          </li>
-          <li className="hover:underline">
-            <a href="https://www.instagram.com/psh.ieee/" target="_blank">
-              Instagram
-            </a>
-          </li>
-        </ul>
-
-        <ul className="space-y-3">
-          <p className=" font-bold">Directory</p>
-          <li className="hover:underline">
-            <Link href={siteConfig.paths.home}>Home</Link>
-          </li>
-          <li className="hover:underline">
-            <Link href={siteConfig.paths.about}>About</Link>
-          </li>
-          <li className="hover:underline">
-            <Link href={siteConfig.paths.partners}>Partners</Link>
-          </li>
-        </ul>
-
-        <ul className="space-y-3">
-          <p className=" font-bold">Legal</p>
-          <li className="hover:underline">
-            <Link href={siteConfig.paths.privacy_policy}>Privacy Policy</Link>
-          </li>
-          <li className="hover:underline">
-            <Link href={siteConfig.paths.terms_of_service}>
-              Terms of Service
-            </Link>
-          </li>
-        </ul>
-
-        <ul className="space-y-3">
-          <p className=" font-bold">Blog</p>
-          <li className="hover:underline">
-            <a href="https://edu.ieee.org/us-psu/" target="_blank">
-              PSH IEEE
-            </a>
-          </li>
-        </ul>
-      </div>
-    </div>
-  );
-}
-
-export function MainFooter() {
-  return (
-    <div className="absolute bottom-0 hidden w-full bg-background py-16 sm:block">
-      <div className="container flex max-w-[80rem] flex-row items-start justify-center text-foreground lg:justify-between">
+    <div className="absolute bottom-0 w-full bg-background py-16 sm:block">
+      <div className="container max-w-[80rem] items-start text-foreground sm:flex sm:flex-row sm:justify-center lg:justify-between">
         <Link
           href={siteConfig.paths.home}
           className="hidden origin-top-left scale-[2] items-center space-x-2 lg:flex"
@@ -84,83 +17,67 @@ export function MainFooter() {
             {siteConfig.name}
           </span>
         </Link>
-        <div className="flex flex-row space-x-16 md:space-x-28 ">
-          <div>
-            <ul className="space-y-3">
-              <p className=" font-bold">Follow Us</p>
-              <li className="hover:underline">
-                <a
-                  href="https://www.linkedin.com/company/ieee-penn-state-harrisbrurg/"
-                  target="_blank"
-                >
-                  LinkedIn
-                </a>
-              </li>
-              <li className="hover:underline">
-                <a
-                  href="https://www.facebook.com/groups/183119638427039/"
-                  target="_blank"
-                >
-                  Facebook
-                </a>
-              </li>
-              <li className="hover:underline">
-                <a href="https://www.instagram.com/psh.ieee/" target="_blank">
-                  Instagram
-                </a>
-              </li>
-            </ul>
-          </div>
-          <div>
-            <ul className="space-y-3">
-              <p className=" font-bold">Directory</p>
-              <li className="hover:underline">
-                <Link href={siteConfig.paths.home}>Home</Link>
-              </li>
-              <li className="hover:underline">
-                <Link href={siteConfig.paths.about}>About</Link>
-              </li>
-              <li className="hover:underline">
-                <Link href={siteConfig.paths.partners}>Partners</Link>
-              </li>
-            </ul>
-          </div>
-          <div>
-            <ul className="space-y-3">
-              <p className=" font-bold">Legal</p>
-              <li className="hover:underline">
-                <Link href={siteConfig.paths.privacy_policy}>
-                  Privacy Policy
-                </Link>
-              </li>
-              <li className="hover:underline">
-                <Link href={siteConfig.paths.terms_of_service}>
-                  Terms of Service
-                </Link>
-              </li>
-            </ul>
-          </div>
-          <div>
-            <ul className="space-y-3">
-              <p className=" font-bold">Blog</p>
-              <li className="hover:underline">
-                <a href="https://edu.ieee.org/us-psu/" target="_blank">
-                  PSH IEEE
-                </a>
-              </li>
-            </ul>
-          </div>
+        <div className="grid grid-cols-2 gap-8 whitespace-nowrap sm:flex sm:w-[39.5rem] sm:flex-row sm:justify-between">
+          <ul className="space-y-3">
+            <p className=" font-bold">Follow Us</p>
+            <li className="hover:underline">
+              <a
+                href="https://www.linkedin.com/company/ieee-penn-state-harrisbrurg/"
+                target="_blank"
+              >
+                LinkedIn
+              </a>
+            </li>
+            <li className="hover:underline">
+              <a
+                href="https://www.facebook.com/groups/183119638427039/"
+                target="_blank"
+              >
+                Facebook
+              </a>
+            </li>
+            <li className="hover:underline">
+              <a href="https://www.instagram.com/psh.ieee/" target="_blank">
+                Instagram
+              </a>
+            </li>
+          </ul>
+
+          <ul className="space-y-3">
+            <p className=" font-bold">Directory</p>
+            <li className="hover:underline">
+              <Link href={siteConfig.paths.home}>Home</Link>
+            </li>
+            <li className="hover:underline">
+              <Link href={siteConfig.paths.partners}>Partners</Link>
+            </li>
+            <li className="hover:underline">
+              <Link href={siteConfig.paths.about}>About</Link>
+            </li>
+          </ul>
+
+          <ul className="space-y-3">
+            <p className=" font-bold">Legal</p>
+            <li className="hover:underline">
+              <Link href={siteConfig.paths.privacy_policy}>Privacy Policy</Link>
+            </li>
+            <li className="hover:underline">
+              <Link href={siteConfig.paths.terms_of_service}>
+                Terms of Service
+              </Link>
+            </li>
+          </ul>
+
+          <ul className="space-y-3">
+            <p className=" font-bold">Blog</p>
+            <li className="hover:underline">
+              <a href="https://edu.ieee.org/us-psu/" target="_blank">
+                PSH IEEE
+              </a>
+            </li>
+          </ul>
         </div>
       </div>
     </div>
-  );
-}
-
-export default function Footer() {
-  return (
-    <>
-      <MainFooter />
-      <MobileFooter />
-    </>
   );
 }
