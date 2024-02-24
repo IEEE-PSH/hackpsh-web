c9d013b848d9b19dd13b266986079fb41926708f
John Nguyen <69529980+ImJunny@users.noreply.github.com>
2024-02-24 02:13:25 +0000

    change nav height to 64px

--- src/app/_components/nav/site-header.tsx
+++ src/app/_components/nav/site-header.tsx
@@ -2,8 +2,8 @@ import { type ReactNode } from "react";
 
 export function SiteHeader({ children }: { children: ReactNode }) {
   return (
-    <div className="z-10 sticky top-0 w-full bg-background">
-      <div className="container flex h-14 max-w-[80rem] items-center">
+    <div className="sticky top-0 z-10 w-full border-b-[1px] bg-background shadow-sm">
+      <div className="container flex h-16 max-w-[80rem] items-center">
         {children}
       </div>
     </div>
