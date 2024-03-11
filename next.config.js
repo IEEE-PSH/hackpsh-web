4dd1f95fa5b6c0c75c29706c50c11356df9f72ff
Dinesh Umasankar <dqu5021@psu.edu>
2024-03-10 23:30:59 -0400

    Remove Comments - Image Optimization in NextConfig

--- next.config.js
+++ next.config.js
@@ -1,12 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // //add images loaded by NextJS; currently a premature optimization
-  // images:{
-  //   loader: 'penn-state-logo',
-  //   loaderFile: ''
-
-  // },
-  // ...
   webpack: (config, { webpack }) => {
       config.plugins.push(new webpack.IgnorePlugin({
           resourceRegExp: /^pg-native$|^cloudflare:sockets$/,
