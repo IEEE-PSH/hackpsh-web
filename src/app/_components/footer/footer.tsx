e32fa36f2e8183a274d5e9b8aff0ec5f6a5ce8a8
Dinesh Umasankar <dinesh71uma@gmail.com>
2024-03-21 01:57:46 -0400

    Footer Touchups

--- src/app/_components/footer/footer.tsx
+++ src/app/_components/footer/footer.tsx
@@ -17,20 +17,20 @@ export default function Footer() {
           </Link>
         </Button>
         <div className="text-xl text-center gap-x-20 gap-y-8 grid grid-cols-2 md:grid-cols-4">
-          <ul className="space-y-2">
-            <p className="font-bold">Follow Us</p>
+          <ul className="space-y-4">
+            <p className="text-2xl font-bold">Follow Us</p>
             <li className="flex flex-col space-y-2">
-              <Button variant="inline_link" size="navigation" className="font-normal" asChild>
+              <Button variant="inline_link" size="navigation" className="text-base font-normal" asChild>
                 <Link href={siteConfig.links.linkedin} target="_blank">
                   LinkedIn
                 </Link>
               </Button>
-              <Button variant="inline_link" size="navigation" className="font-normal" asChild>
+              <Button variant="inline_link" size="navigation" className="text-base font-normal" asChild>
                 <Link href={siteConfig.links.facebook} target="_blank">
                   Facebook
                 </Link>
               </Button>
-              <Button variant="inline_link" size="navigation" className="font-normal" asChild>
+              <Button variant="inline_link" size="navigation" className="text-base font-normal" asChild>
                 <Link href={siteConfig.links.instagram} target="_blank">
                   Instagram
                 </Link>
@@ -38,37 +38,37 @@ export default function Footer() {
             </li>
           </ul>
 
-          <ul className="space-y-2">
-            <p className="font-bold">Directory</p>
+          <ul className="space-y-4">
+            <p className="text-2xl font-bold">Directory</p>
             <li className="flex flex-col space-y-2">
-              <Button variant="inline_link" size="navigation" className="font-normal" asChild>
+              <Button variant="inline_link" size="navigation" className="text-base font-normal" asChild>
                 <Link href={siteConfig.paths.home}>Home</Link>
               </Button>
-              <Button variant="inline_link" size="navigation" className="font-normal" asChild>
+              <Button variant="inline_link" size="navigation" className="text-base font-normal" asChild>
                 <Link href={siteConfig.paths.partners}>Partners</Link>
               </Button>
-              <Button variant="inline_link" size="navigation" className="font-normal" asChild>
+              <Button variant="inline_link" size="navigation" className="text-base font-normal" asChild>
                 <Link href={siteConfig.paths.about}>About</Link>
               </Button>
             </li>
           </ul>
 
-          <ul className="space-y-2">
-            <p className="font-bold">Legal</p>
+          <ul className="space-y-4">
+            <p className="text-2xl font-bold">Legal</p>
             <li className="flex flex-col space-y-2">
-              <Button variant="inline_link" size="navigation" className="font-normal" asChild>
+              <Button variant="inline_link" size="navigation" className="text-base font-normal" asChild>
                 <Link href={siteConfig.paths.privacy_policy}>Privacy Policy</Link>
               </Button>
-              <Button variant="inline_link" size="navigation" className="font-normal" asChild>
+              <Button variant="inline_link" size="navigation" className="text-base font-normal" asChild>
                 <Link href={siteConfig.paths.terms_of_service}>Terms of Service</Link>
               </Button>
             </li>
           </ul>
 
-          <ul className="space-y-2">
-            <p className="font-bold">Blog</p>
+          <ul className="space-y-4">
+            <p className="text-2xl font-bold">Blog</p>
             <li className="flex flex-col space-y-2">
-              <Button variant="inline_link" size="navigation" className="font-normal" asChild>
+              <Button variant="inline_link" size="navigation" className="text-base font-normal" asChild>
                 <Link href={siteConfig.links.blog} target="_blank">
                   PSH IEEE
                 </Link>
