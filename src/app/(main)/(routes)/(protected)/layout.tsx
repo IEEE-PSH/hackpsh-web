2678b371237740f00178d108495f8ffc8c9d4cb6
John Nguyen <69529980+ImJunny@users.noreply.github.com>
2024-03-23 14:19:01 +0000

    remove horizontal scroll

--- src/app/(main)/(routes)/(protected)/layout.tsx
+++ src/app/(main)/(routes)/(protected)/layout.tsx
@@ -2,7 +2,7 @@ import ProtectedSiteHeader from "@/app/_components/nav/protected-site-header";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="w-screen min-h-screen">
+    <div className="min-h-screen">
       <ProtectedSiteHeader />
       {children}
     </div>
