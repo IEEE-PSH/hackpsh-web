99193fef2f43681f4de358b40b81333eca9830cd
John Nguyen <69529980+ImJunny@users.noreply.github.com>
2024-03-23 01:02:40 -0400

    feat: Add Internal Challenges Card to Challenges (HackerRank) (#69)
    * add hackerrank problem
    
    * change rule

--- src/app/_config/site.ts
+++ src/app/_config/site.ts
@@ -3,13 +3,15 @@ export const siteConfig = {
   emails: {
     general: "ieee@hackpsh.org",
   },
-  registration_form: "https://docs.google.com/forms/d/e/1FAIpQLSdkVRX1e8lNruVzWVKgcQ6YOt9NWhkotzOysjU8YdewjDvvTA/viewform",
+  registration_form:
+    "https://docs.google.com/forms/d/e/1FAIpQLSdkVRX1e8lNruVzWVKgcQ6YOt9NWhkotzOysjU8YdewjDvvTA/viewform",
   location: "https://maps.app.goo.gl/EEXBSXZNzEgM6dhv7",
   links: {
     linkedin: "https://www.linkedin.com/company/ieee-penn-state-harrisbrurg/",
     facebook: "https://www.facebook.com/groups/183119638427039/",
     instagram: "https://www.instagram.com/psh.ieee/",
     blog: "https://edu.ieee.org/us-psu/",
+    hackerrank: "https://www.hackerrank.com/hackpsh-spring-2024",
   },
   paths: {
     home: "/",
