62ffe82b17e928d51b49fd53dc4f08c6faa9c775
John Nguyen <johnnguyen4501@gmail.com>
2024-09-16 20:46:00 -0400

    Revert "restructure realtime leaderboard"
    This reverts commit 4afb420b533310af325aa71511137e78ed3f6b66.

--- src/app/_components/leaderboard/realtime-leaderboard.tsx
+++ src/app/_components/leaderboard/realtime-leaderboard.tsx
@@ -2,37 +2,30 @@
 
 import { type LeaderboardStandings } from "@/server/dao/leaderboard";
 import DataTable from "./data-table";
-import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
-import { useEffect } from "react";
-import { useRouter } from "next/navigation";
 
 export default function RealtimeLeaderboard({
   serverData,
 }: {
   serverData: LeaderboardStandings;
 }) {
-  const supabase = createClientComponentClient();
-  const router = useRouter();
+  // const supabase = createClientComponentClient();
 
-  useEffect(() => {
-    const channel = supabase.channel("leaderboard").on(
-      "postgres_changes",
-      {
-        event: "*",
-        schema: "app_schema",
-        table: "app_team",
-      },
-      (payload) => {
-        //refresh route on app_team changes to update leaderboard ui
-        router.refresh();
-      },
-    );
+  // useEffect(() => {
+  //   const channel = supabase.channel("leaderboard")
+  //     .on('postgres_changes',
+  //       {
+  //         event: '*',
+  //         schema: 'app_schema',
+  //         table: 'app_team'
+  //       },
+  //       (payload) => console.log(payload)
+  //     );
 
-    channel.subscribe();
-    // return () => {
-    //   supabase.removeChannel(channel)
-    // }
-  }, []);
+  //   channel.subscribe();
+  //   return () => {
+  //     supabase.removeChannel(channel)
+  //   }
+  // }, []);
 
-  return <DataTable data={serverData} className="max-w-4xl" />;
+  return <DataTable data={serverData} className="max-w-[55rem]" />;
 }
