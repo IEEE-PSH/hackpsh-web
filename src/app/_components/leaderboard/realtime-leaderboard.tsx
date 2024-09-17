4afb420b533310af325aa71511137e78ed3f6b66
John Nguyen <johnnguyen4501@gmail.com>
2024-09-16 20:04:53 -0400

    restructure realtime leaderboard

--- src/app/_components/leaderboard/realtime-leaderboard.tsx
+++ src/app/_components/leaderboard/realtime-leaderboard.tsx
@@ -2,30 +2,37 @@
 
 import { type LeaderboardStandings } from "@/server/dao/leaderboard";
 import DataTable from "./data-table";
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
 
 export default function RealtimeLeaderboard({
   serverData,
 }: {
   serverData: LeaderboardStandings;
 }) {
-  // const supabase = createClientComponentClient();
+  const supabase = createClientComponentClient();
+  const router = useRouter();
 
-  // useEffect(() => {
-  //   const channel = supabase.channel("leaderboard")
-  //     .on('postgres_changes',
-  //       {
-  //         event: '*',
-  //         schema: 'app_schema',
-  //         table: 'app_team'
-  //       },
-  //       (payload) => console.log(payload)
-  //     );
+  useEffect(() => {
+    const channel = supabase.channel("leaderboard").on(
+      "postgres_changes",
+      {
+        event: "*",
+        schema: "app_schema",
+        table: "app_team",
+      },
+      (payload) => {
+        //refresh route on app_team changes to update leaderboard ui
+        router.refresh();
+      },
+    );
 
-  //   channel.subscribe();
-  //   return () => {
-  //     supabase.removeChannel(channel)
-  //   }
-  // }, []);
+    channel.subscribe();
+    // return () => {
+    //   supabase.removeChannel(channel)
+    // }
+  }, []);
 
-  return <DataTable data={serverData} className="max-w-[55rem]" />;
+  return <DataTable data={serverData} className="max-w-4xl" />;
 }
