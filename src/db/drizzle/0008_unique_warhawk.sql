8627819226e2e94b41764fbc84ae87a942493e0c
John Nguyen <johnnguyen4501@gmail.com>
2024-09-16 20:22:52 -0400

    add RLS policy to app_team

--- src/db/drizzle/0008_unique_warhawk.sql
+++ src/db/drizzle/0008_unique_warhawk.sql
@@ -6,3 +6,5 @@ ALTER TABLE "app_schema"."app_school_year" ENABLE ROW LEVEL SECURITY;
 ALTER TABLE "app_schema"."app_team" ENABLE ROW LEVEL SECURITY;
 ALTER TABLE "app_schema"."app_user_profile" ENABLE ROW LEVEL SECURITY;
 ALTER TABLE "drizzle"."__drizzle_migrations" ENABLE ROW LEVEL SECURITY;
+
+CREATE POLICY "Enable read access for all users" ON "app_schema"."app_team" AS PERMISSIVE FOR SELECT TO authenticated USING (true);
