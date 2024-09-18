7d656409e070f7e644f1b7fff68ba44dfbf942cf
John Nguyen <johnnguyen4501@gmail.com>
2024-09-18 05:00:40 -0400

    remove sidenav to test deployment

--- src/app/(main)/(routes)/(protected)/layout.tsx
+++ src/app/(main)/(routes)/(protected)/layout.tsx
@@ -5,7 +5,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="min-h-screen bg-background">
       <div className="flex">
-        <ProtectedSideNav />
+        {/* <ProtectedSideNav /> */}
         <div className="w-full">
           <ProtectedSiteHeader />
           <div>{children}</div>
