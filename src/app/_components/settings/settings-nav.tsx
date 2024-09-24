da2045b24e9ad409ebf0a16794c46b797277374b
John Nguyen <johnnguyen4501@gmail.com>
2024-09-24 03:28:13 -0400

    only display settings nav for admin

--- src/app/_components/settings/settings-nav.tsx
+++ src/app/_components/settings/settings-nav.tsx
@@ -5,7 +5,7 @@ import { siteConfig } from "@/app/_config/site";
 import { cn } from "@/app/_lib/client-utils";
 import { usePathname } from "next/navigation";
 
-export default function SettingsNav() {
+export default function SettingsNav({ userRole }: { userRole: string }) {
   const pathname = usePathname();
   return (
     <div className="container my-4 flex space-x-2">
@@ -24,51 +24,57 @@ export default function SettingsNav() {
           <span>Account</span>
         </Link>
       </Button>
-      <Button
-        className={cn(
-          pathname === siteConfig.paths.users
-            ? "rounded-full bg-accent text-accent-foreground"
-            : "text-foreground/60 hover:bg-accent/50",
-          "rounded-full",
-        )}
-        variant="ghost"
-        size="default"
-        asChild
-      >
-        <Link href={siteConfig.paths.users} scroll={true}>
-          <span>Users</span>
-        </Link>
-      </Button>
-      <Button
-        className={cn(
-          pathname === siteConfig.paths.teams
-            ? "rounded-full bg-accent text-accent-foreground"
-            : "text-foreground/60 hover:bg-accent/50",
-          "rounded-full",
-        )}
-        variant="ghost"
-        size="default"
-        asChild
-      >
-        <Link href={siteConfig.paths.teams} scroll={true}>
-          <span>Teams</span>
-        </Link>
-      </Button>
-      <Button
-        className={cn(
-          pathname === siteConfig.paths.event
-            ? "bg-accent text-accent-foreground"
-            : "text-foreground/60 hover:bg-accent/50",
-          "rounded-full",
-        )}
-        variant="ghost"
-        size="default"
-        asChild
-      >
-        <Link href={siteConfig.paths.event} scroll={true}>
-          <span>Event</span>
-        </Link>
-      </Button>
+      {userRole === "admin" ? (
+        <>
+          <Button
+            className={cn(
+              pathname === siteConfig.paths.users
+                ? "rounded-full bg-accent text-accent-foreground"
+                : "text-foreground/60 hover:bg-accent/50",
+              "rounded-full",
+            )}
+            variant="ghost"
+            size="default"
+            asChild
+          >
+            <Link href={siteConfig.paths.users} scroll={true}>
+              <span>Users</span>
+            </Link>
+          </Button>
+          <Button
+            className={cn(
+              pathname === siteConfig.paths.teams
+                ? "rounded-full bg-accent text-accent-foreground"
+                : "text-foreground/60 hover:bg-accent/50",
+              "rounded-full",
+            )}
+            variant="ghost"
+            size="default"
+            asChild
+          >
+            <Link href={siteConfig.paths.teams} scroll={true}>
+              <span>Teams</span>
+            </Link>
+          </Button>
+          <Button
+            className={cn(
+              pathname === siteConfig.paths.event
+                ? "bg-accent text-accent-foreground"
+                : "text-foreground/60 hover:bg-accent/50",
+              "rounded-full",
+            )}
+            variant="ghost"
+            size="default"
+            asChild
+          >
+            <Link href={siteConfig.paths.event} scroll={true}>
+              <span>Event</span>
+            </Link>
+          </Button>
+        </>
+      ) : (
+        <></>
+      )}
     </div>
   );
 }
