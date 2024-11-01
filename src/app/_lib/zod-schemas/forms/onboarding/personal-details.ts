7a2e06ea475ddc1bada152e4938fc6548d8ba93f
John Nguyen <johnnguyen4501@gmail.com>
2024-11-01 01:41:53 -0400

    Modify zod schemas for onboarding

--- src/app/_lib/zod-schemas/forms/onboarding/personal-details.ts
+++ src/app/_lib/zod-schemas/forms/onboarding/personal-details.ts
@@ -1,15 +1,14 @@
-import { dbMajors, dbSchoolYear } from "@/db/drizzle/startup_seed";
 import { z } from "zod";
 
 export const PersonalDetailsFormSchema = z.object({
+  user_first_name: z.string().min(1, { message: "Cannot leave field empty." }),
+  user_last_name: z.string().min(1, { message: "Cannot leave field empty." }),
   user_display_name: z
     .string()
     .min(5, { message: "Display Name must be 5 or more characters long." })
     .max(30, {
       message: "Display Name must be fewer than 30 characters long.",
     }),
-  user_school_year: z.enum(dbSchoolYear),
-  user_major: z.enum(dbMajors),
 });
 
 export type TPersonalDetailsForm = z.infer<typeof PersonalDetailsFormSchema>;
