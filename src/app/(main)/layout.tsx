f086b4234157e81b11b1cfaea84d5ebbf383587e
John Nguyen <johnnguyen4501@gmail.com>
2024-12-06 11:47:50 -0500

    Remove Speed Insights

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -6,7 +6,6 @@ import { Toaster } from "@/app/_components/ui/toaster";
 import ReactQueryProvider from "@/app/_trpc/react";
 import { headers } from "next/headers";
 import NextTopLoader from "nextjs-toploader";
-import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const font = GeistSans;
 
@@ -26,7 +25,6 @@ export default function RootLayout({
         <link rel="icon" type="image/svg+xml" href="/brand.svg" />
       </head>
       <body className={font.className}>
-        <SpeedInsights />
         <NextThemesProvider
           attribute="class"
           defaultTheme="dark"
