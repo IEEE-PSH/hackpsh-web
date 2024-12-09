84790a747ff5ce559c98416e2ad52db473033edb
John Nguyen <69529980+ImJunny@users.noreply.github.com>
2024-12-09 16:46:27 +0000

    Refactor sidenav

--- src/app/(main)/(routes)/(protected)/(pages)/layout.tsx
+++ src/app/(main)/(routes)/(protected)/(pages)/layout.tsx
@@ -1,6 +1,5 @@
 import AnnouncementNotifer from "@/app/_components/announcement/announcement-notifier";
 import EventUpdateNotifer from "@/app/_components/event/event-update-notifier";
-import { ProtectedMiniSideNav } from "@/app/_components/nav/protected-mini-side-nav";
 import { ProtectedSideNav } from "@/app/_components/nav/protected-side-nav";
 import ProtectedSiteHeader from "@/app/_components/nav/protected-site-header";
 
@@ -14,7 +13,6 @@ export default function Layout({ children }: { children: React.ReactNode }) {
         <ProtectedSiteHeader />
         <div>
           <ProtectedSideNav />
-          <ProtectedMiniSideNav />
           <div className="py-4 md:ml-[72px] xl:ml-72">{children}</div>
         </div>
       </div>
