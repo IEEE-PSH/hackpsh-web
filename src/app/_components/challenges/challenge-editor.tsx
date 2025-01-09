cc7572aa46a6fb3950ee0bb4b6405d18b1ab782d
John Nguyen <johnnguyen4501@gmail.com>
2025-01-09 06:51:48 -0500

    test codemirror

--- src/app/_components/challenges/challenge-editor.tsx
+++ src/app/_components/challenges/challenge-editor.tsx
@@ -6,17 +6,15 @@ import React, {
   useRef,
   useState,
 } from "react";
-import { Controlled as CodeMirror2 } from "react-codemirror2";
-import CodeMirror, { EditorView, ViewUpdate } from "@uiw/react-codemirror";
-import { javascript } from "@codemirror/lang-javascript";
-import { vscodeDark, vscodeLight } from "@uiw/codemirror-theme-vscode";
+
 import { type TLanguages } from "@/server/zod-schemas/challenges";
 import { trpc } from "@/app/_trpc/react";
-import { useTheme } from "next-themes";
 import { useChallenge } from "./challenge-context-provider";
-import { io, type Socket } from "socket.io-client";
-import { cpp } from "@codemirror/lang-cpp";
-import { python } from "@codemirror/lang-python";
+import * as Y from "yjs";
+import { WebrtcProvider } from "y-webrtc";
+import { type editor } from "monaco-editor";
+import { useTheme } from "next-themes";
+import CodeMirror from "codemirror";
 
 type ChallengeEditor = {
   value: string;
@@ -27,34 +25,15 @@ type ChallengeEditor = {
   solved: boolean;
 };
 
-function debounce<T extends unknown[]>(
-  func: (...args: T) => void,
-  delay: number,
-): (...args: T) => void {
-  let debounceTimer: NodeJS.Timeout | null = null;
-
-  return (...args: T) => {
-    if (debounceTimer) {
-      clearTimeout(debounceTimer);
-    }
-    debounceTimer = setTimeout(() => {
-      func(...args);
-    }, delay);
-  };
-}
-
 export default function ChallengeEditorWrapper({
   value,
   setValue,
-  language,
   setLanguage,
   header,
-  solved,
 }: ChallengeEditor) {
   const { theme } = useTheme();
   const { userData, challengeData } = useChallenge();
   const [isFetched, setIsFetched] = useState<boolean>(false);
-  const [existingData, setExistingData] = useState<boolean>(true);
   const { data: submission } = trpc.challenges.get_code_submission.useQuery(
     {
       challenge_id: challengeData?.challenge_id as unknown as number,
@@ -81,78 +60,77 @@ export default function ChallengeEditorWrapper({
     if (value.length > 0) setValue(value);
   }, []);
 
-  // SOCKET LOGIC BELOW
-  const [socket, setSocket] = useState<Socket | null>(null);
+  // SOCKET LOGIC BELOW------------------------------------------------
   const roomName = `${userData?.user_team_name}s${challengeData?.challenge_id}`;
+  // const editorRef = useRef<editor.IStandaloneCodeEditor | null>(null);
+  // const docRef = useRef<Y.Doc | null>(null);
+  // const providerRef = useRef<WebrtcProvider | null>(null);
 
+  // function handleEditorDidMount(editor: editor.IStandaloneCodeEditor) {
+  //   editorRef.current = editor;
+  //   if (editorRef.current && !docRef.current) {
+  //     const doc = new Y.Doc();
+
+  //     const provider = new WebrtcProvider(roomName, doc, {
+  //       signaling: ["wss://sly-living-goose.glitch.me"],
+  //     });
+
+  //     docRef.current = doc;
+  //     providerRef.current = provider;
+
+  //     const type = doc.getText("monaco");
+
+  //     // Bind Monaco editor to Yjs document
+  //     const binding = new MonacoBinding(
+  //       type,
+  //       editorRef.current.getModel(),
+  //       new Set([editorRef.current]),
+  //       provider.awareness,
+  //     );
+  //   }
+  // }
+  const docRef = useRef<Y.Doc | null>(null);
+  const providerRef = useRef<WebrtcProvider | null>(null);
   useEffect(() => {
-    const s = io(process.env.NEXT_PUBLIC_GLITCH_WSS, {
-      transports: ["websocket"],
-      withCredentials: true,
-    });
-    setSocket(s);
+    try {
+      if (!docRef.current) {
+        const ydoc = new Y.Doc();
+        docRef.current = ydoc;
 
-    s.on("connect", () => {
-      s.emit("joinRoom", { room_name: roomName, header: header });
-    });
+        const provider = new WebrtcProvider(roomName, ydoc, {
+          // signaling: ["wss://sly-living-goose.glitch.me"],
+        });
+        const yText = ydoc.getText("codemirror");
+        const yUndoManager = new Y.UndoManager(yText);
 
-    if (!existingData) setValue(header);
-    return () => {
-      s.off("update");
-      s.disconnect();
-    };
+        const e = new CodeMirror(document.getElementById("editor"), {
+          mode: "simplemode",
+          lineNumbers: true,
+          theme: "material-darker",
+        });
+        window.binding = new CodemirrorBinding(yText, e, provider.awareness, {
+          yUndoManager,
+        });
+        console.log("connect to", room);
+        window.binding.awareness.setLocalStateField("user", {
+          color: color,
+          name: name,
+        });
+      }
+    } catch (error) {
+      console.log(error);
+    }
   }, [roomName]);
 
-  const editorRef = useRef<EditorView>(null);
-  useEffect(() => {
-    if (!socket) return;
-    socket.on("update", (newContent: string) => {
-      if (newContent !== value) {
-        // const view = editorRef.current.view;
-        // const currentPosition = view.state.selection.main.head;
-        setValue(newContent);
-        // requestAnimationFrame(() => {
-        //   const transaction = view.state.update({
-        //     selection: { anchor: currentPosition },
-        //   });
-        //   view.dispatch(transaction);
-        // });
-      }
-    });
-
-    socket.on("existingData", (content: string) => {
-      setExistingData(true);
-      setValue(content);
-    });
-  }, [socket, value]);
-
-  const handleOnChange = debounce((newValue: string, view: ViewUpdate) => {
-    if (socket) {
-      setValue(newValue);
-      socket.emit("update", { room_name: roomName, content: newValue });
-    }
-  }, 120);
-
   return (
-    <div className="h-full min-h-[400px]" style={{ height: "100%" }}>
-      <CodeMirror
-        ref={editorRef}
-        value={value}
-        height="100%"
-        theme={theme === "dark" ? vscodeDark : vscodeLight}
-        extensions={[
-          language === "javascript"
-            ? javascript()
-            : language === "cpp"
-              ? cpp()
-              : python(),
-        ]}
-        onChange={(newValue, viewUpdate) =>
-          handleOnChange(newValue, viewUpdate)
-        }
-        style={{ height: "100%", fontSize: 14 }}
-        readOnly={solved}
-      />
+    <div className="h-full min-h-[400px]">
+      <div id="editor"></div>
+      {/* <Editor
+        onMount={handleEditorDidMount}
+        options={{
+          theme: theme === "dark" ? "vs-dark" : "light",
+        }}
+      /> */}
     </div>
   );
 }
